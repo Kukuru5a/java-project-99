0908e4c4680f565e9635c4fe14cc53a1ed105015
kukuru5a <romanzag@icloud.com>
2024-01-08 09:49:16 +0600

    new update for entire app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,48 +1,48 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.2.1"
-	id("io.spring.dependency-management") version "1.1.4"
+    java
+    id("org.springframework.boot") version "3.2.1"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "hexlet.code"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_20
+    sourceCompatibility = JavaVersion.VERSION_20
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-devtools")
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-configuration-processor")
 
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 
-	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
-	implementation("org.mapstruct:mapstruct:1.6.0.Beta1")
-	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.0.Beta1")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+    implementation("org.mapstruct:mapstruct:1.6.0.Beta1")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.0.Beta1")
 
-	implementation("org.instancio:instancio-junit:3.6.0")
-	implementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
-	implementation("net.datafaker:datafaker:2.0.2")
+    implementation("org.instancio:instancio-junit:3.6.0")
+    implementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+    implementation("net.datafaker:datafaker:2.0.2")
 
-	runtimeOnly("com.h2database:h2:2.2.224")
-	testImplementation("org.springframework.security:spring-security-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation(platform("org.junit:junit-bom:5.10.1"))
-	testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+    runtimeOnly("com.h2database:h2:2.2.224")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
