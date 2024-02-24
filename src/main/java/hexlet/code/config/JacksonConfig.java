ed5ae304a0b606e5eb0845e366b697908dfcbf3e
kukuru5a <romanzag@icloud.com>
2024-02-25 04:25:58 +0600

    fixes13

--- src/main/java/hexlet/code/config/JacksonConfig.java
+++ src/main/java/hexlet/code/config/JacksonConfig.java
@@ -19,26 +19,4 @@ public class JacksonConfig {
         builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
         return builder;
     }
-//    @Bean
-//    Jackson2ObjectMapperBuilder objectMapperBuilder() {
-//        var builder = new Jackson2ObjectMapperBuilder();
-//        builder.serializationInclusion(JsonInclude.Include.NON_NULL)
-//                .modulesToInstall(new JsonNullableModule())
-//                .serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
-//                .featuresToEnable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT)
-//                .featuresToEnable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
-//        return builder;
-//    }
-//    @Bean
-//    public Jackson2ObjectMapperBuilder objectMapperBuilder() {
-//        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-//        builder.serializerByType(LocalDateTime.class, new JsonSerializer<LocalDateTime>() {
-//            @Override
-//            public void serialize(LocalDateTime value, JsonGenerator gen,
-//                                  SerializerProvider serializers) throws IOException {
-//                gen.writeNumber(value.toEpochSecond(ZoneOffset.UTC));
-//            }
-//        });
-//        return builder;
-//    }
 }
